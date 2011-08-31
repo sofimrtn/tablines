cf60e5a6b9fb8a823e3c444b6dc067f9843b84e4
alfonsoNoriega <alfonsoNoriega@localhost>
2011-08-31 10:28:23 +0200

    Added Basic structure for Even, Binding and BindingList

--- tabels-core/src/main/scala/es/ctic/tabels/Interpreter.scala
+++ tabels-core/src/main/scala/es/ctic/tabels/Interpreter.scala
@@ -5,6 +5,11 @@ abstract class Interpreter {
   type RDFgraph
   
   def interpret(root : S, dataSource: DataSource) : RDFgraph 
-  
 
 }
+
+case class Event (bindingsList : BindingList)
+
+case class Binding(label : String  , value : CellValue)
+
+case class BindingList(bindingsList : List[Binding] = List()) 
