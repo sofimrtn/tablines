63c16cb0cb558d7ea6d4e3cac41dbf65730924eb
alfonsoNoriega <alfonsoNoriega@localhost>
2011-08-31 12:41:36 +0200

    Added Evaluable abstract class to the grammar

--- tabels-core/src/main/scala/es/ctic/tabels/GrammarModel.scala
+++ tabels-core/src/main/scala/es/ctic/tabels/GrammarModel.scala
@@ -1,45 +1,32 @@
 package es.ctic.tabels
 
-case class S (patternList: List[Pattern], templateList : List[Template])
+case class S (patternList: List[Pattern], templateList : List[Template]) extends Evaluable
 
 case class Pattern (letE : LetWhereExpression,var lBindE : List[BindingExpresion] = List() , 
-					lPatternM : List[PatternMatch], whereE : LetWhereExpression)
+					lPatternM : List[PatternMatch], whereE : LetWhereExpression) extends Evaluable
 
-case class LetWhereExpression(sentList : List[Assignment]) 
+case class LetWhereExpression(sentList : List[Assignment]) extends Evaluable
 
-case class BindingExpresion(dim : Dimension, filterCondList: List[FilterCondition] = List(), pos : Position = null, stopCond : StopCondition = null, variable: Var = null)
+case class BindingExpresion(dim : Dimension, filterCondList: List[FilterCondition] = List(), 
+		pos : Position = null, stopCond : StopCondition = null, variable: Var = null) extends Evaluable
 
-case class PatternMatch(filterCondList: List[FilterCondition] = List(), pos : Position = null, stopCond : StopCondition = null, variable: Var = null, tupple : Tupple = null)
+case class PatternMatch(filterCondList: List[FilterCondition] = List(), pos : Position = null, 
+		stopCond : StopCondition = null, variable: Var = null, tupple : Tupple = null) extends Evaluable
 
-case class Dimension {
+case class Dimension (dim : String) extends Evaluable
 
-}
+case class FilterCondition (cond : String) extends Evaluable
 
-case class FilterCondition {
+case class Position (pos : String) extends Evaluable
 
-}
+case class StopCondition (cond: String) extends Evaluable
 
-case class Position {
+case class Var (name : String) extends Evaluable
 
-}
+case class Tupple( varList: List[Var]) extends Evaluable
 
-case class StopCondition {
+case class Expression (exp: String) extends Evaluable
 
-}
+case class Assignment(v : Var, e : Expression) extends Evaluable
 
-case class Var {
-
-}
-
-case class Tupple( varList: List[Var]){
-
-}
-
-case class Expression {
-
-}
-
-case class Assignment(v : Var, e : Expression){
-  
-    
-}
+abstract class Evaluable
