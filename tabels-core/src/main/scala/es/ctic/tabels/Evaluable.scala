069592a9c63b49cb6e40a6870e4e54708b2a6814
alfonsoNoriega <alfonsoNoriega@localhost>
2011-09-01 15:36:45 +0200

    Added more details to evaluabe class

--- tabels-core/src/main/scala/es/ctic/tabels/Evaluable.scala
+++ tabels-core/src/main/scala/es/ctic/tabels/Evaluable.scala
@@ -1,17 +1,20 @@
 package es.ctic.tabels
 
 trait Evaluable{
-	def grammarEvaluation( evaluationContext : EvaluationContext, dataSource: DataSource): Event = this match{
+	
+  def grammarEvaluation( evaluationContext : EvaluationContext, dataSource: DataSource): Event = this match{
 	  
+     case Pattern(letE, lBindE, lPatternM , whereE)=>
+	    print("Pattern ")
+	    lBindE.foreach(pattern => grammarEvaluation(evaluationContext, dataSource) :: evaluationContext.eventList)
+	    return new Event(new BindingList(List(new Binding("$prueba", dataSource.getValue(new Point("","",0,0))))))
+    
 	  case S(patternList, templateList )=>
-	    print("S := ")
+	    print("S := 1")
 	    patternList.foreach(pattern => grammarEvaluation(evaluationContext, dataSource) :: evaluationContext.eventList)
 	    return new Event(new BindingList(List(new Binding("$prueba", dataSource.getValue(new Point("","",0,0))))))
 	    
-	  case Pattern(letE, lBindE,lPatternM , whereE)=>
-	    print("S := ")
-	    lBindE.foreach(pattern => grammarEvaluation(evaluationContext, dataSource) :: evaluationContext.eventList)
-	    return new Event(new BindingList(List(new Binding("$prueba", dataSource.getValue(new Point("","",0,0))))))
+	 
 	    
 	  case LetWhereExpression(sentList)=>
 	    print(" LET/Where")
@@ -22,6 +25,6 @@ trait Evaluable{
 	    filterCondList.foreach(condition => grammarEvaluation(evaluationContext, dataSource) :: evaluationContext.eventList)
 	    return new Event(new BindingList(List(new Binding("$prueba", dataSource.getValue(new Point("","",0,0)))))) 
 	    
-	  
+	 case _  => return new Event(new BindingList(List(new Binding("$prueba", dataSource.getValue(new Point("","",0,0))))))
 	}
 }
