4891a6ac2703ee3bd4e9f44310e916c73f481e97
alfonsoNoriega <alfonsoNoriega@localhost>
2011-09-02 15:37:03 +0200

    Added functionality to Template class instantiate method

--- tabels-core/src/main/scala/es/ctic/tabels/Evaluable.scala
+++ tabels-core/src/main/scala/es/ctic/tabels/Evaluable.scala
@@ -4,6 +4,6 @@ trait Evaluable{
 	
   def grammarEvaluation( evaluationContext : EvaluationContext, dataSource: DataSource): Event = this match{
 	  
-     case _  => return new Event(new BindingList(List(new Binding("$prueba", dataSource.getValue(new Point("","",0,0))))))
+     case _  => return new Event(new BindingList(List(new Binding("$prueba", dataSource.getValue(new Point("hola.xls","Hoja1",0,0))))))
 	}
 }
