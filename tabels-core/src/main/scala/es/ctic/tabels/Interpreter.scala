e1afe9b1beceb0e4d79ce31879186380d0f2b5d3
alfonsoNoriega <alfonsoNoriega@localhost>
2011-09-06 12:10:11 +0200

    Modified interpret method: added buffer list

--- tabels-core/src/main/scala/es/ctic/tabels/Interpreter.scala
+++ tabels-core/src/main/scala/es/ctic/tabels/Interpreter.scala
@@ -1,12 +1,16 @@
 package es.ctic.tabels
+import scala.collection.mutable.ListBuffer
 
 case class Interpreter {
   
-  def interpret(root : S, dataSource: DataSource,dataOut : DataOutput) = {
+  def interpret(root : S, dataSource: DataSource, dataOut : DataOutput) = {
     
+    //FIX IT//
     var evContext = new EvaluationContext()
-    evContext.eventList :::= root.grammarEvaluation(evContext, dataSource)
-    val template = new Template(evContext.eventList)
+    evContext.buffList += root.grammarEvaluation(evContext, dataSource)
+    evContext.eventList= evContext.buffList.toList 
+   
+    val template = new Template(evContext.eventList, dataOut)
 	template.instantiate()  
   }
 
@@ -16,4 +20,6 @@ case class Event (bindingList : List[Binding])
 
 case class Binding(label : String  , value : String)
 
-case class EvaluationContext(pointList : List[Point] = List() , var eventList: List[Event] = List())
+class EvaluationContext(pointList : List[Point] = List() , var eventList: List[Event] = null){
+	var buffList = new ListBuffer[Event]
+}
