2cdff82ecba81bb7f459f5baed96a693a686f735
alfonsoNoriega <alfonsoNoriega@localhost>
2011-09-06 12:12:10 +0200

    Moved JenaDataOutput subclass to a new file

--- tabels-core/src/main/scala/es/ctic/tabels/DataOutput.scala
+++ tabels-core/src/main/scala/es/ctic/tabels/DataOutput.scala
@@ -3,12 +3,5 @@ package es.ctic.tabels
 
 abstract class DataOutput  {
 
-	def generateStatement(statement : Statement)
-}
-
-case class RDFDataOutput(tripletList:StatementList) extends DataOutput{
-  
-  def generateStatement(statement: Statement){ 
-	  
-  }
+	def generateOutput(statement : Statement)
 }
