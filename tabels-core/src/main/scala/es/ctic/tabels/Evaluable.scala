36401c0aac4e3a57f4349db80d8e5edb92a4b842
alfonsoNoriega <alfonsoNoriega@localhost>
2011-09-06 12:11:37 +0200

    Modified grammarEvaluation method  functionality: added buffer list

--- tabels-core/src/main/scala/es/ctic/tabels/Evaluable.scala
+++ tabels-core/src/main/scala/es/ctic/tabels/Evaluable.scala
@@ -1,27 +1,30 @@
 package es.ctic.tabels
+import scala.collection.mutable.ListBuffer
 
 trait Evaluable{
 	
-  def grammarEvaluation( evaluationContext : EvaluationContext, dataSource: DataSource): List[Event] = this match{
+  def grammarEvaluation( evaluationContext : EvaluationContext, dataSource: DataSource): Event = this match{
 	  
+     
+  //FIX IT//
      case _:S  =>
        this.asInstanceOf[S].patternList.foreach(patt => 
-         		evaluationContext.eventList :::= patt.grammarEvaluation(evaluationContext,dataSource) )
-       return evaluationContext.eventList ::: List(new Event(List(new Binding("$prueba", dataSource.getValue(new Point("horas.xls","Hoja1",0,0)).getContent))))
+         		evaluationContext.buffList += patt.grammarEvaluation(evaluationContext,dataSource) )
+       return new Event(List(new Binding("$prueba", dataSource.getValue(new Point("horas.xls","Hoja1",0,0)).getContent)))
      
      case _:Pattern  => 
-       evaluationContext.eventList :::= this.asInstanceOf[Pattern].letE.grammarEvaluation(evaluationContext, dataSource)
-       evaluationContext.eventList :::= this.asInstanceOf[Pattern].whereE.grammarEvaluation(evaluationContext, dataSource)
-       return evaluationContext.eventList ::: List(new Event(List(new Binding("$pattern", dataSource.getValue(new Point("horas.xls","Hoja1",0,0)).getContent))))
+       evaluationContext.buffList += this.asInstanceOf[Pattern].letE.grammarEvaluation(evaluationContext, dataSource)
+       evaluationContext.buffList += this.asInstanceOf[Pattern].whereE.grammarEvaluation(evaluationContext, dataSource)
+       return new Event(List(new Binding("$pattern", dataSource.getValue(new Point("horas.xls","Hoja1",0,0)).getContent)))
 	   
      
      case _:LetWhereExpression  => 
-       var bList =List (Binding)
+       var bList =new ListBuffer[Binding]
        this.asInstanceOf[LetWhereExpression].sentList.foreach(sent =>
-         						bList:::= new Binding(sent.v.name, sent.e.exp))
+         						bList += new Binding(sent.v.name, sent.e.exp))
          						
-       return List(new Event(bList))  
+       return new Event(bList.toList)   
        
-     case _  => return List(new Event(List(new Binding("$nomatch", dataSource.getValue(new Point("horas.xls","Hoja1",0,0)).getContent))))
+     case _  => return new Event(List(new Binding("$nomatch", dataSource.getValue(new Point("horas.xls","Hoja1",0,0)).getContent)))
   }
 }
