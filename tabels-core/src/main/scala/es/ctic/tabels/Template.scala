22e8b9eabe525cca87c486bb939e64d7e922527d
alfonsoNoriega <alfonsoNoriega@localhost>
2011-09-06 15:33:38 +0200

    Modified instantiate method from Template class

--- tabels-core/src/main/scala/es/ctic/tabels/Template.scala
+++ tabels-core/src/main/scala/es/ctic/tabels/Template.scala
@@ -6,9 +6,16 @@ case class Statement(subject: String, property: String, obj:String){
 
 case class Template(eventList : List[Event], dataOut: DataOutput) {
   	
-	def instantiate()/*:StatemenList*/ = {
+	def instantiate() = {
 	
 	  
-	  eventList.foreach(event => event.bindingList.foreach(binding => print(binding.label)))
+	  eventList.foreach(event => event.bindingList.foreach(binding =>
+	    							{
+	    							  dataOut.generateOutput(new Statement(binding.label,"http://example/friends", binding.value)) 
+	    							  println(dataOut.asInstanceOf[JenaDataOutput].model)
+	    							 }
+	    							)
+	    				)
 	}
+	  					
 }
