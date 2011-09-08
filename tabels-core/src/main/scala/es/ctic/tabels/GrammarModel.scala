ab7cdbbe0142a29dcec078b75cb91784df8e84b0
Diego Berrueta <diego@berrueta.net>
2011-09-08 17:52:46 +0200

    Renamed model class

--- tabels-core/src/main/scala/es/ctic/tabels/GrammarModel.scala
+++ tabels-core/src/main/scala/es/ctic/tabels/GrammarModel.scala
@@ -5,7 +5,7 @@ case class S (var patternList: List[Pattern], var templateList : List[Template])
 case class Pattern (letE : LetWhereExpression,var lBindE : List[BindingExpresion] = List() , 
 					lPatternM : List[PatternMatch], whereE : LetWhereExpression) extends Evaluable
 
-case class LetWhereExpression(sentList : List[Assingment]) extends Evaluable
+case class LetWhereExpression(sentList : List[Assignment]) extends Evaluable
 
 case class BindingExpresion(dim : Dimension, filterCondList: List[FilterCondition] = List(), 
 		pos : Position = null, stopCond : StopCondition = null, variable: Variable = null) extends Evaluable
@@ -27,5 +27,5 @@ case class Tuple(variables : List[Variable]) extends Evaluable
 
 case class Expression (exp: String) extends Evaluable
 
-case class Assingment(variable : Variable, expression : Expression) extends Evaluable
+case class Assignment(variable : Variable, expression : Expression) extends Evaluable
 
