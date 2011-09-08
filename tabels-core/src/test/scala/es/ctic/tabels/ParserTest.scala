688b208ebac5b001d0cce735a0cf8d8e087d3978
Diego Berrueta <diego@berrueta.net>
2011-09-08 17:42:54 +0200

    Added parser test

---
+++ tabels-core/src/test/scala/es/ctic/tabels/ParserTest.scala
@@ -0,0 +1,50 @@
+package es.ctic.tabels
+
+import org.scalatest.junit.JUnitSuite
+import scala.collection.mutable.ListBuffer
+import org.junit.Test
+import org.junit.Assert._
+import scala.util.parsing.combinator._
+import scala.util.parsing.input.CharSequenceReader
+
+class TabelsParserTest extends TabelsParser with JUnitSuite {
+    
+    @Test def parseVariable() {
+        assertParse(variable, "?x", Var("?x"))
+        assertParse(variable, "?X", Var("?X"))
+        assertParse(variable, "?xx", Var("?xx"))
+        assertParse(variable, "?x0y", Var("?x0y"))
+        assertFail (variable, "")
+        assertFail (variable, "?")
+        assertFail (variable, "??x")
+        assertFail (variable, "?0")
+    }
+    
+    @Test def parsePosition() {
+        assertParse(position, "A1", Position("A1"))
+        assertParse(position, "BC10", Position("BC10"))
+        assertFail (position, "")
+        assertFail (position, "A")
+        assertFail (position, "1")
+        assertFail (position, "1A")
+    }
+
+    // auxiliary methods inspired by
+    // http://henkelmann.eu/2011/01/29/an_introduction_to_scala_parser_combinators-part_3_unit_tests
+    private def assertParse[T](p:Parser[T], input:String, expectedValue: T) = {
+        val phraseParser = phrase(p)
+        phraseParser(new CharSequenceReader(input)) match {
+            case Success(t,_)     => assertEquals("While parsing '" + input + "'", expectedValue, t)
+            case NoSuccess(msg,_) => throw new IllegalArgumentException("Could not parse '" + input + "': " + msg)
+        }
+    }
+
+    private def assertFail[T](p:Parser[T],input:String) = {
+        val phraseParser = phrase(p)
+        phraseParser(new CharSequenceReader(input)) match {
+            case Success(t,_)     => fail()
+            case NoSuccess(msg,_) => msg
+        }
+    }
+
+}
