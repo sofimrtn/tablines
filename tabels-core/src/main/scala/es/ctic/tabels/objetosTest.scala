ad8886e7381df55baf03dd8b9128f75d646c1707
alfonsoNoriega <alfonsoNoriega@localhost>
2011-09-09 09:06:05 +0200

    Modified objects for test cases

--- tabels-core/src/main/scala/es/ctic/tabels/objetosTest.scala
+++ tabels-core/src/main/scala/es/ctic/tabels/objetosTest.scala
@@ -1,7 +1,7 @@
 package es.ctic.tabels
 
 object AST1 extends PatternMatch(List(),Position("A1"),null, Var("?x"),null ) 
-object DS1 extends ExcelDataSource() 
+object DS1 extends ExcelDataSource(List("horas.xls")) 
 object VIS1 extends VisitorToString()
 object VIS2 extends VisitorEvaluate(DS1)
 object POINT extends Point("horas.xls", "Hoja1" , 0, 0)
