e5fef0c6987610f3892003dffe4aeb430a027a70
alfonsoNoriega <alfonsoNoriega@localhost>
2011-09-26 18:03:08 +0200

    Optimization access speed to data source

--- tabels-core/src/main/scala/es/ctic/tabels/ExcelDataSource.scala
+++ tabels-core/src/main/scala/es/ctic/tabels/ExcelDataSource.scala
@@ -1,6 +1,7 @@
 package es.ctic.tabels
 
 import java.io.File
+import scala.collection.mutable.HashMap
 import jxl._
 import java.util.Arrays
 import grizzled.slf4j.Logging
@@ -11,10 +12,21 @@ class ExcelDataSource(fl : Seq[File]) extends DataSource with Logging {
 	private val files : Seq[File] = fl
 	
 	val filenames : Seq[String] = files.map(_.getName())
+	
+	private val workbooks = new HashMap[File, Workbook]()
+	
+	private def getWorkbook(file : File) : Workbook = {
+	   workbooks.get(file) match {
+	     case Some(workbook) => return workbook
+	     case None => val workbook = Workbook.getWorkbook(file)
+	                  workbooks.put(file, workbook)
+	                  return workbook
+	   }
+	}
   
   override def getValue(point : Point) : CellValue = {
 	logger.debug("Getting value at " + point)
-    val workbook : Workbook = Workbook.getWorkbook(new File (point.path) )
+    val workbook : Workbook = getWorkbook(new File (point.path) )
 	val sheet : Sheet = workbook.getSheet(point.tab)
 	val cell : Cell = sheet.getCell(point.row, point.col)
     return ExcelCellValue(cell)
@@ -22,7 +34,7 @@ class ExcelDataSource(fl : Seq[File]) extends DataSource with Logging {
   
   override def getTabs(filename : String) : Seq[String] = {
 	 
-    val workbook : Workbook = Workbook.getWorkbook(new File (filename) )
+    val workbook : Workbook = getWorkbook(new File (filename) )
     val sheetNames : Array[String] = workbook.getSheetNames()
     val listSheets : java.util.List[String] = new java.util.LinkedList()
     sheetNames.foreach(sheet => listSheets.add(sheet))
@@ -31,14 +43,14 @@ class ExcelDataSource(fl : Seq[File]) extends DataSource with Logging {
   
   override def getRows(filename : String, tabName : String) : Int = {
     
-    val workbook : Workbook = Workbook.getWorkbook(new File (filename))
+    val workbook : Workbook = getWorkbook(new File (filename))
     val sheet : Sheet = workbook.getSheet(tabName)
 
     return sheet.getRows()
 }
   override def getCols(filename : String, tabName : String) : Int = {
     
-    val workbook : Workbook = Workbook.getWorkbook(new File (filename))
+    val workbook : Workbook = getWorkbook(new File (filename))
     val sheet : Sheet = workbook.getSheet(tabName)
 
     return sheet.getColumns()
