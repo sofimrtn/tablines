d62efc16d49c3c4250a377191f47576330a95e23
Diego Berrueta <diego@berrueta.net>
2011-10-15 09:53:54 +0200

    Extracted RDF code to a new file

---
+++ tabels-core/src/main/scala/es/ctic/tabels/RDF.scala
@@ -0,0 +1,52 @@
+package es.ctic.tabels
+
+abstract class RDFNode() {
+    
+	def asBoolean : Literal
+	def asString : Literal
+	def +(suffix : String) : RDFNode
+	
+}
+
+case class Literal(value : String, rdfType: Resource = XSD_STRING, langTag : String = "") extends RDFNode {
+	
+	def truthValue : Boolean = Set("true", "1") contains this.asBoolean.value
+
+	override def +(suffix : String) : Literal = Literal(this.value + suffix)
+
+    /**
+     * This method calculates the effective boolean value of the
+     * literal by applying the rules of fn:boolean, see
+     * http://www.w3.org/TR/rdf-sparql-query/#ebv
+     */
+	override def asBoolean : Literal = rdfType match {
+	    case XSD_BOOLEAN => this
+	    case XSD_STRING =>  if (value.length > 0) LITERAL_TRUE else LITERAL_FALSE
+	    case XSD_INT | XSD_DOUBLE | XSD_DECIMAL | XSD_FLOAT => if (value.toDouble == 0.0) LITERAL_FALSE else LITERAL_TRUE
+	}
+	
+	override def asString : Literal = Literal(value)
+	
+}
+
+case class Resource(uri : String) extends RDFNode {
+	override def asBoolean : Literal = LITERAL_TRUE
+	override def asString : Literal = Literal(uri)
+	override def +(suffix : String) : Resource = Resource(this.uri + suffix)
+}
+
+object RDF_TYPE extends Resource("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
+object XSD_STRING extends Resource("http://www.w3.org/2001/XMLSchema#string")
+object XSD_BOOLEAN extends Resource("http://www.w3.org/2001/XMLSchema#boolean")
+object XSD_INT extends Resource("http://www.w3.org/2001/XMLSchema#int")
+object XSD_DOUBLE extends Resource("http://www.w3.org/2001/XMLSchema#double")
+object XSD_FLOAT extends Resource("http://www.w3.org/2001/XMLSchema#float")
+object XSD_DECIMAL extends Resource("http://www.w3.org/2001/XMLSchema#decimal")
+object LITERAL_TRUE extends Literal("true", XSD_BOOLEAN)
+object LITERAL_FALSE extends Literal("false", XSD_BOOLEAN)
+
+
+// FIXME: literals can not be properties
+case class Statement(subject: RDFNode, property: RDFNode, obj:RDFNode){
+
+}
--- tabels-core/src/test/scala/es/ctic/tabels/TemplateTest.scala
+++ tabels-core/src/test/scala/es/ctic/tabels/TemplateTest.scala
@@ -4,32 +4,10 @@ import org.scalatest.junit.JUnitSuite
 import org.junit.Test
 import org.junit.Assert._
 
-class LiteralTest extends JUnitSuite {
-  
-  @Test def truthValue {
-      assertTrue(LITERAL_TRUE.truthValue)
-      assertFalse(LITERAL_FALSE.truthValue)
-      assertTrue(Literal("true", rdfType = XSD_BOOLEAN).truthValue)
-      assertTrue(Literal("1", rdfType = XSD_BOOLEAN).truthValue)    
-      assertFalse(Literal("false", rdfType = XSD_BOOLEAN).truthValue)
-      assertFalse(Literal("0", rdfType = XSD_BOOLEAN).truthValue)    
-  }
-  
-  @Test def asBoolean {
-      // see http://www.w3.org/TR/rdf-sparql-query/#ebv for details
-      assertEquals(LITERAL_TRUE, LITERAL_TRUE.asBoolean)
-      assertEquals(LITERAL_FALSE, LITERAL_FALSE.asBoolean)
-      assertEquals(LITERAL_TRUE, Literal("hello").asBoolean)
-      assertEquals(LITERAL_FALSE, Literal("").asBoolean)
-      assertEquals(LITERAL_TRUE, Literal("5", XSD_INT).asBoolean)
-      assertEquals(LITERAL_TRUE, Literal("5.5", XSD_DOUBLE).asBoolean)
-      assertEquals(LITERAL_TRUE, Literal("5.5", XSD_FLOAT).asBoolean)
-      assertEquals(LITERAL_TRUE, Literal("5.5", XSD_DECIMAL).asBoolean)
-      assertEquals(LITERAL_FALSE, Literal("0", XSD_INT).asBoolean)
-      assertEquals(LITERAL_FALSE, Literal("0.0", XSD_DOUBLE).asBoolean)
-      assertEquals(LITERAL_FALSE, Literal("0.0", XSD_FLOAT).asBoolean)
-      assertEquals(LITERAL_FALSE, Literal("0.0", XSD_DECIMAL).asBoolean)
-  }
+//class TemplateTest extends JUnitSuite {
+//  
+//}
 
-}
-
+//class TripleTemplateTest extends JUnitSuite {
+//
+//}
