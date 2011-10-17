20153606a2cd5aa8ae0ac9912939b5dfedc3b50a
Diego Berrueta <diego@berrueta.net>
2011-10-17 22:16:04 +0200

    Simplified positions

--- tabels-core/src/main/scala/es/ctic/tabels/Position.scala
+++ tabels-core/src/main/scala/es/ctic/tabels/Position.scala
@@ -2,44 +2,36 @@ package es.ctic.tabels
 
 import es.ctic.tabels.RelativePos._
 
-abstract class Position(r: Int, c : Int) extends EvaluablePosition {
+abstract class Position extends EvaluablePosition {
   
-	val row = r
-	val col = c
-	def accept(vis : Visitor) 
 	def getPosition(evaluationContext: EvaluationContext) : Point
 
 }
-case class FixedPosition (override val row : Int , override val col: Int) extends Position(row , col) {
-	
-	override def toString() : String = columnConverter.intToAlpha(col) + (row+1)
-	
-	override def getPosition(evaluationContext: EvaluationContext) : Point = Point(evaluationContext.getValue(Dimension.files),evaluationContext.getValue(Dimension.sheets),col,row)
-	
-	override def accept(vis : Visitor) = {
-	    
-	    vis.visit(this)
-	  }
-}
 
-case class WithVariablePosition (variable : Variable) extends Position(0,0) {
+case class FixedPosition (row : Int, col: Int) extends Position {
 	
 	override def toString() : String = columnConverter.intToAlpha(col) + (row+1)
 	
-	override def getPosition(evaluationContext: EvaluationContext) : Point = evaluationContext.bindings.getPoint(variable)
-	override def accept(vis : Visitor) = {
-	    
-	    vis.visit(this)
-	  }
-}
-
-case class RelativePosition (relativity : RelativePos, reference:Position, displacement: Int) extends Position(0,0) {
-	
-	override def toString() : String = columnConverter.intToAlpha(col) + (row+1)
-	
-	override def getPosition(evaluationContext: EvaluationContext) : Point ={
+	override def getPosition(evaluationContext: EvaluationContext) : Point =
 	  Point(evaluationContext.getValue(Dimension.files),evaluationContext.getValue(Dimension.sheets),col,row)
-	  relativity match{
+	
+}
+
+case class WithVariablePosition (variable : Variable) extends Position {
+	
+	override def toString() : String = variable.toString
+	
+	override def getPosition(evaluationContext: EvaluationContext) : Point =
+	  evaluationContext.bindings.getPoint(variable)
+
+}
+
+case class RelativePosition (relativity : RelativePos, reference:Position, displacement: Int) extends Position {
+	
+	override def toString() : String = displacement.toString + " " + relativity + " " + reference.toString
+	
+	override def getPosition(evaluationContext: EvaluationContext) : Point = {
+	  relativity match {
 	    case RelativePos.top =>  Point((reference.getPosition(evaluationContext)).path,(reference.getPosition(evaluationContext)).tab,(reference.getPosition(evaluationContext)).col,(reference.getPosition(evaluationContext)).row - displacement)
 	    case RelativePos.bottom =>  Point((reference.getPosition(evaluationContext)).path,(reference.getPosition(evaluationContext)).tab,(reference.getPosition(evaluationContext)).col,(reference.getPosition(evaluationContext)).row + displacement)
 	    case RelativePos.left =>  Point((reference.getPosition(evaluationContext)).path,(reference.getPosition(evaluationContext)).tab,(reference.getPosition(evaluationContext)).col - displacement,(reference.getPosition(evaluationContext)).row)
@@ -48,9 +40,5 @@ case class RelativePosition (relativity : RelativePos, reference:Position, displ
 	
 	}
 	
-	override def accept(vis : Visitor) = {
-	    
-	    vis.visit(this)
-	  }
 }
 
