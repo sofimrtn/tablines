e612826fc687e9d79666f2ff966864993f736617
Diego Berrueta <diego@berrueta.net>
2011-10-22 20:34:42 +0200

    Pretty print expressions

--- tabels-core/src/main/scala/es/ctic/tabels/Expression.scala
+++ tabels-core/src/main/scala/es/ctic/tabels/Expression.scala
@@ -4,41 +4,50 @@ import scala.util.matching.Regex
 abstract class Expression {
   
   def evaluate(evaluationContext : EvaluationContext) : RDFNode
+  
+  def prettyPrint() : String
+  
+  override def toString = prettyPrint
 
 }
 
 case class VariableReference(variable:Variable) extends Expression{
   
   override def evaluate(evaluationContext : EvaluationContext) = evaluationContext.bindings.getValue(variable)
+  override def prettyPrint = variable.toString
 
 }
 
 case class AddVariableExpression(variable : Variable, expression: Expression) extends Expression{
   
   override def evaluate(evaluationContext : EvaluationContext) = evaluationContext.bindings.getValue(variable) + expression.evaluate(evaluationContext).asString.value.toString
+  override def prettyPrint = variable.toString + "+" + expression.toString
 
 }
 
 case class ResourceExpression(expression:Expression, uri : Resource) extends Expression {
   
   override def evaluate(evaluationContext : EvaluationContext) = uri + expression.evaluate(evaluationContext).asString.value.toString
+  override def prettyPrint = "resource(" + expression.toString + "," + uri.toString + ")"
 
 }
 
 case class LiteralExpression(literal : Literal) extends Expression{
     
   override def evaluate(evaluationContext : EvaluationContext) = literal
+  override def prettyPrint = literal.toString
 
 }
 
 case class RegexExpression(expression : Expression , re : Regex) extends Expression{
   
 	override def evaluate(evaluationContext : EvaluationContext) =
-	   
 	 expression.evaluate(evaluationContext).asString.value.toString.matches(re.toString()) match{
 	    case true =>  LITERAL_TRUE
 	    case false => LITERAL_FALSE
 	  }
+    override def prettyPrint = "matches(" + expression.toString + ",\"" + re.toString + "\")"
+
 }
 
 case class NotExpression(expression:Expression) extends Expression{
@@ -49,17 +58,24 @@ case class NotExpression(expression:Expression) extends Expression{
 	    LITERAL_FALSE
 	 else
 		LITERAL_TRUE
+
+    override def prettyPrint = "not(" + expression.toString + ")"
+
 }
 
 /* *Type change expressions  * */
 case class BooleanE(expression: Expression) extends Expression{
   
   override def evaluate(evaluationContext : EvaluationContext) =  Literal(expression.evaluate(evaluationContext).asBoolean.value, XSD_BOOLEAN)
+  override def prettyPrint = "boolean(" + expression.toString + ")"
+  
 }
 
 case class StringE(expression: Expression) extends Expression{
   
   override def evaluate(evaluationContext : EvaluationContext) = Literal(expression.evaluate(evaluationContext).asString.value, XSD_STRING)
+  override def prettyPrint = "string(" + expression.toString + ")"
+
 }
 //FIX ME: Type control
 case class IntE(expression : Expression) extends Expression{
@@ -71,6 +87,7 @@ case class IntE(expression : Expression) extends Expression{
     	case _ => throw new InvalidTypeFunctionException("Is not posible to generate Int RDF type with this Literal") 
     }
   }   
+  override def prettyPrint = "int(" + expression.toString + ")"
 	
 }
 case class FloatE(expression : Expression) extends Expression{
@@ -82,6 +99,7 @@ case class FloatE(expression : Expression) extends Expression{
     	case _ => throw new InvalidTypeFunctionException("Is not posible to generate Float RDF type with this Literal") 
     }
   }   
+  override def prettyPrint = "float(" + expression.toString + ")"
 	
 }
 
@@ -94,5 +112,6 @@ case class DecimalE(expression : Expression) extends Expression{
     	case _ => throw new InvalidTypeFunctionException("Is not posible to generate Decimal RDF type with this Literal") 
     }
   }   
+  override def prettyPrint = "decimal(" + expression.toString + ")"
 	
 }
