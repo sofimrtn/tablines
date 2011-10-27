5d0f099075816eae7edf2ca07be823ef540f6336
Diego Berrueta <diego@berrueta.net>
2011-10-27 22:59:56 +0200

    Untested project controller (RDF generation)

--- tabels-web/grails-app/controllers/es/ctic/tabels/ProjectController.groovy
+++ tabels-web/grails-app/controllers/es/ctic/tabels/ProjectController.groovy
@@ -1,9 +1,24 @@
 package es.ctic.tabels
 
+import scala.collection.JavaConversions$
+
 class ProjectController {
 
     def index = { }
     
-    def rdf = { }
+    def rdf = {
+        def workDir = "/tmp/tabels/"
+        def tabelsFilename = "default.tabels"
+        def spreadsheetFiles = new File(workDir).listFiles().collect { it.name =~ ".*xls\$" }
+        def dataSource = new ExcelDataSource(spreadsheetFiles)
+		def parser = new TabelsParser()
+        def programFile = new File(workDir + tabelsFilename)
+        def program = (programFile.exists()) ? parser.parseProgram(programFile) : Autogenerator$.autogenerateProgram(dataSource)
+		def interpreter = new Interpreter()
+		def dataOutput = new JenaDataOutput(/* Map() ++ program.prefixes */)
+		interpreter.interpret(program, dataSource, dataOutput)
+		def os = System.out
+        dataOutput.model.write(os, "RDF/XML")
+    }
     
 }
