9a8c9f3137f07ebcc92e05d29dd9fc44ffe68200
Diego Berrueta <diego@berrueta.net>
2011-10-27 21:48:21 +0200

    Added dependency to tabels-core

--- tabels-web/grails-app/conf/BuildConfig.groovy
+++ tabels-web/grails-app/conf/BuildConfig.groovy
@@ -27,5 +27,6 @@ grails.project.dependency.resolution = {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
         // runtime 'mysql:mysql-connector-java:5.1.13'
+        compile 'es.ctic.tabels:tabels-core_2.9.1:0.1'
     }
 }
