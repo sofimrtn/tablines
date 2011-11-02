fd97da56993916d546390e15321e040c34e9abc9
Diego Berrueta <diego@berrueta.net>
2011-11-02 20:39:20 +0100

    Fixed test

--- tabels-core/src/test/scala/es/ctic/tabels/ExcelDataSourceTest.scala
+++ tabels-core/src/test/scala/es/ctic/tabels/ExcelDataSourceTest.scala
@@ -5,51 +5,50 @@ import org.junit.{Test,Before}
 import org.junit.Assert._
 import java.io.File
 
-class ExcelDataSourceIntegrationTest extends JUnitSuite {
+class ExcelDataAdapterIntegrationTest extends JUnitSuite {
 	
-	var dataSource : ExcelDataSource = null
+	var dataAdapter : ExcelDataAdapter = null
 	val filename1 : String = this.getClass.getResource("/es/ctic/tabels/Test1.xls").getFile
 	val sheet1 = "Hoja1"
 	
 	@Before def setUp {
 		val file1 = new File(filename1)
-		val files = Seq(file1)
-		dataSource = new ExcelDataSource(files)
+		dataAdapter = new ExcelDataAdapter(file1)
 	}
 	
 	@Test def getTabs {
-		assertEquals(Seq("Hoja1", "Hoja2", "Hoja3"), dataSource.getTabs(filename1))
+		assertEquals(Seq("Hoja1", "Hoja2", "Hoja3"), dataAdapter.getTabs())
 	}
 	
 	@Test def getCols {
-		assertEquals(2, dataSource.getCols(filename1, sheet1))
+		assertEquals(2, dataAdapter.getCols(sheet1))
 	}
 	
 	@Test def getRows {
-		assertEquals(11, dataSource.getRows(filename1, sheet1))
+		assertEquals(11, dataAdapter.getRows(sheet1))
 	}
 	
 	@Test def getValue {
-		assertEquals(Literal("Formatted"), dataSource.getValue(Point(filename1, sheet1, row = 0, col = 1)).getContent)
+		assertEquals(Literal("Formatted"), dataAdapter.getValue(Point(filename1, sheet1, row = 0, col = 1)).getContent)
 
-		assertEquals(Literal("3.1415", XSD_DECIMAL), dataSource.getValue(Point(filename1, sheet1, row = 1, col = 1)).getContent)
-		assertEquals(Literal("3", XSD_INT), dataSource.getValue(Point(filename1, sheet1, row = 2, col = 1)).getContent)
-		assertEquals(Literal("3.01", XSD_DECIMAL), dataSource.getValue(Point(filename1, sheet1, row = 3, col = 1)).getContent)
-		assertEquals(Literal("-5", XSD_INT), dataSource.getValue(Point(filename1, sheet1, row = 4, col = 1)).getContent)
-		assertEquals(Literal("38281827", XSD_INT), dataSource.getValue(Point(filename1, sheet1, row = 5, col = 1)).getContent)
+		assertEquals(Literal("3.1415", XSD_DECIMAL), dataAdapter.getValue(Point(filename1, sheet1, row = 1, col = 1)).getContent)
+		assertEquals(Literal("3", XSD_INT), dataAdapter.getValue(Point(filename1, sheet1, row = 2, col = 1)).getContent)
+		assertEquals(Literal("3.01", XSD_DECIMAL), dataAdapter.getValue(Point(filename1, sheet1, row = 3, col = 1)).getContent)
+		assertEquals(Literal("-5", XSD_INT), dataAdapter.getValue(Point(filename1, sheet1, row = 4, col = 1)).getContent)
+		assertEquals(Literal("38281827", XSD_INT), dataAdapter.getValue(Point(filename1, sheet1, row = 5, col = 1)).getContent)
 
-		assertEquals(Literal("6", XSD_INT), dataSource.getValue(Point(filename1, sheet1, row = 6, col = 1)).getContent)
-		assertEquals(Literal("0.31", XSD_DECIMAL), dataSource.getValue(Point(filename1, sheet1, row = 7, col = 1)).getContent)
+		assertEquals(Literal("6", XSD_INT), dataAdapter.getValue(Point(filename1, sheet1, row = 6, col = 1)).getContent)
+		assertEquals(Literal("0.31", XSD_DECIMAL), dataAdapter.getValue(Point(filename1, sheet1, row = 7, col = 1)).getContent)
 //FIX ME: TimeZone 
-//		assertEquals(Literal("2011-10-13", XSD_DATE), dataSource.getValue(Point(filename1, sheet1, row = 8, col = 1)).getContent)
+//		assertEquals(Literal("2011-10-13", XSD_DATE), dataAdapter.getValue(Point(filename1, sheet1, row = 8, col = 1)).getContent)
 
-		assertEquals(Literal("6", XSD_INT), dataSource.getValue(Point(filename1, sheet1, row = 9, col = 1)).getContent)
-		assertEquals(Literal("9", XSD_STRING), dataSource.getValue(Point(filename1, sheet1, row = 10, col = 1)).getContent)
+		assertEquals(Literal("6", XSD_INT), dataAdapter.getValue(Point(filename1, sheet1, row = 9, col = 1)).getContent)
+		assertEquals(Literal("9", XSD_STRING), dataAdapter.getValue(Point(filename1, sheet1, row = 10, col = 1)).getContent)
 	}
 	
 }
 
-class ExcelDataSourceTest extends JUnitSuite {
+class ExceldataAdapterTest extends JUnitSuite {
 
 	@Test def decimalFormatPattern {
 	     assertFalse( "Testing '0'",ExcelCellValue(null).decimalFormatPattern.findFirstIn("0").isEmpty)
