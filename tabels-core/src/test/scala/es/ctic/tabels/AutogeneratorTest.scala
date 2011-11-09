18c1b33d58ba093bb03d7b5997e165e67e016c0b
alfonsoNoriega <alfonsoNoriega@localhost>
2011-11-09 10:51:01 +0100

    Added Levenshtein distance unitary test + fixed Autogenerator test

--- tabels-core/src/test/scala/es/ctic/tabels/AutogeneratorTest.scala
+++ tabels-core/src/test/scala/es/ctic/tabels/AutogeneratorTest.scala
@@ -10,8 +10,8 @@ class AutogeneratorTest extends Autogenerator with JUnitSuite {
     
     @Test def testLiteralToLocalName {
         assertEquals(Some("foo"), literalToLocalName(Literal("foo")))
-        assertEquals(Some("foobar"), literalToLocalName(Literal("foo bar")))
-        assertEquals(Some("FOOBAR"), literalToLocalName(Literal("F.//OO(BA)%#R")))
+        assertEquals(Some("fooBar"), literalToLocalName(Literal("foo bar")))
+        assertEquals(Some("fOOBAR"), literalToLocalName(Literal("F.//OO(BA)%#R")))
         assertEquals(None, literalToLocalName(Literal("")))
         assertEquals(Some("n2011"), literalToLocalName(Literal("2011", XSD_INT)))
     }
