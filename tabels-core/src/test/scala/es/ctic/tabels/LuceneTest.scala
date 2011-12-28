743ca060d35910161e63c2c5dfa814bee47f0b6e
alfonsoNoriega <alfonsoNoriega@localhost>
2011-12-28 15:57:09 +0100

    Added Lucene new strategies unitary tests

--- tabels-core/src/test/scala/es/ctic/tabels/LuceneTest.scala
+++ tabels-core/src/test/scala/es/ctic/tabels/LuceneTest.scala
@@ -9,6 +9,14 @@ class LuceneTest extends JUnitSuite{
   @Test def testQueryTest{
     val lucene = new Lucene()
      assertEquals( Some(Resource("http://dbpedia.org/resource/Andorra")),lucene.query("Andorra"))
+     assertEquals( Some(Resource("http://dbpedia.org/resource/Roman")),lucene.query("roma"))
+     assertEquals( Some(Resource("http://dbpedia.org/resource/Madrid")),lucene.query("madrid", "first"))
+     assertEquals( Some(Resource("http://dbpedia.org/resource/Madrid")),lucene.query("madrid", "very-best"))
+     assertEquals( None,lucene.query("casa", "very-best"))
+     assertEquals( None,lucene.query("madrid", "single"))
+     assertEquals( Some(Resource("http://dbpedia.org/resource/Pulmonary_alveolus")),lucene.query("alveolo", "single"))
+     assertEquals( Some(Resource("http://dbpedia.org/resource/Rome,_Open_City")),lucene.query("rome", rdfType=Some(Resource("http://schema.org/Movie"))))
+     
      assertEquals( None,lucene.query("arrimdsf"))
      assertEquals( None,lucene.query(""))
   }
