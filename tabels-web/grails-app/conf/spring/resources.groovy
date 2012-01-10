3e5fe7caa451b6cbcfca0534362a8980ad62cccd
Diego Berrueta <diego@berrueta.net>
2012-01-10 22:51:35 +0100

    Do not use locale

--- tabels-web/grails-app/conf/spring/resources.groovy
+++ tabels-web/grails-app/conf/spring/resources.groovy
@@ -1,14 +1,14 @@
 // Place your Spring DSL code here
 beans = {
     
-    locale(java.util.Locale, "es")
+//    locale(java.util.Locale, "es")
     
     sparqlDataSource(es.ctic.tabels.SparqlDataSourceImpl) {
         projectService = ref("projectService")
     }
     
     datasetProvider(es.ctic.data.tapinos.autocomplete.AutocompleteFromEndpoint, sparqlDataSource, "http://purl.org/NET/scovo#Dataset") {
-		rdfLocale = ref("locale")
+//		rdfLocale = ref("locale")
 		labelProperty = "http://www.w3.org/2004/02/skos/core#prefLabel"
 	}
 
