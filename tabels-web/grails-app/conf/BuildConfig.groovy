8196fc19375ec67fedbdc715c2260160b15275aa
Diego Berrueta <diego@berrueta.net>
2012-01-10 23:25:17 +0100

    Plugin dependencies moved to Groovy file

--- tabels-web/grails-app/conf/BuildConfig.groovy
+++ tabels-web/grails-app/conf/BuildConfig.groovy
@@ -17,13 +17,18 @@ grails.project.dependency.resolution = {
         // uncomment the below to enable remote dependency resolution
         // from public Maven repositories
         mavenLocal()
+        mavenRepo "http://wopr.fundacionctic.org:8081/nexus/content/groups/public"
         mavenCentral()
         //mavenRepo "http://snapshots.repository.codehaus.org"
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
-        mavenRepo "http://wopr.fundacionctic.org:8081/nexus/content/groups/public"
         mavenRepo "http://www.scala-tools.org/repo-releases/"
+    }
+    plugins {
+        runtime ':tapinos-js:1.1',
+                ':uploadr:0.5.0',
+                ':highcharts:2.1.0' // indirect dependency
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
