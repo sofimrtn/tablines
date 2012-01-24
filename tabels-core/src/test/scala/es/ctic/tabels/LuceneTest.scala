745c3e0f46eb724ad5c89a7c65310a121f5bbf4e
Diego Berrueta <diego@berrueta.net>
2012-01-24 21:31:12 +0100

    Fixed compilation error

--- tabels-core/src/test/scala/es/ctic/tabels/LuceneTest.scala
+++ tabels-core/src/test/scala/es/ctic/tabels/LuceneTest.scala
@@ -9,14 +9,14 @@ class LuceneTest extends JUnitSuite{
   @Test def testQueryTest{
     val lucene = new Lucene()
     val evaluationContext: EvaluationContext = null
-     assertEquals( NamedResource("http://dbpedia.org/resource/Andorra"),lucene.query(wA,"Andorra","first"))
-     assertEquals( NamedResource("http://dbpedia.org/resource/Rome"),lucene.query(wA,"roma","first"))
-     assertEquals( NamedResource("http://dbpedia.org/resource/Madrid"),lucene.query(wA,"madrid", "first"))
-     assertEquals( NamedResource("http://dbpedia.org/resource/Madrid"),lucene.query(wA,"madrid", "very-best"))
-     assertEquals( NamedResource("http://example.org/ResourceNotDisambiguated?query=casa"),lucene.query(wA,"casa", "very-best"))
-     assertEquals( NamedResource("http://example.org/ResourceNotDisambiguated?query=madrid"),lucene.query(wA,"madrid", "single"))
-     assertEquals( NamedResource("http://dbpedia.org/resource/Pulmonary_alveolus"),lucene.query(wA,"alveolo", "single"))
-     assertEquals( NamedResource("http://dbpedia.org/resource/Rome,_Open_City"),lucene.query(wA,"rome", rdfType=Some(NamedResource("http://schema.org/Movie"))))
+     assertEquals( NamedResource("http://dbpedia.org/resource/Andorra"),lucene.query(evaluationContext,"Andorra","first"))
+     assertEquals( NamedResource("http://dbpedia.org/resource/Rome"),lucene.query(evaluationContext,"roma","first"))
+     assertEquals( NamedResource("http://dbpedia.org/resource/Madrid"),lucene.query(evaluationContext,"madrid", "first"))
+     assertEquals( NamedResource("http://dbpedia.org/resource/Madrid"),lucene.query(evaluationContext,"madrid", "very-best"))
+     assertEquals( NamedResource("http://example.org/ResourceNotDisambiguated?query=casa"),lucene.query(evaluationContext,"casa", "very-best"))
+     assertEquals( NamedResource("http://example.org/ResourceNotDisambiguated?query=madrid"),lucene.query(evaluationContext,"madrid", "single"))
+     assertEquals( NamedResource("http://dbpedia.org/resource/Pulmonary_alveolus"),lucene.query(evaluationContext,"alveolo", "single"))
+     assertEquals( NamedResource("http://dbpedia.org/resource/Rome,_Open_City"),lucene.query(evaluationContext,"rome", rdfType=Some(NamedResource("http://schema.org/Movie"))))
    
     
      assertEquals( None,lucene.query(evaluationContext,"arrimdsf"))
