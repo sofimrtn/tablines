a0f6698a122c2ffed018d1383ce0ac2c056ebf5d
alfonsoNoriega <alfonsoNoriega@localhost>
2012-01-25 12:28:08 +0100

    Moved miscellanea functions from Expressions file to miscellaneaExpressions classe file

--- tabels-core/src/main/scala/es/ctic/tabels/Expression.scala
+++ tabels-core/src/main/scala/es/ctic/tabels/Expression.scala
@@ -1,7 +1,5 @@
 package es.ctic.tabels
 
-import scala.util.matching.Regex
-import java.net.URLEncoder
 
 abstract class Expression {
   
@@ -147,117 +145,7 @@ trait FunctionCollection {
 
 }
 
-/*
- * TABELS Expressions
- */
 
-object MiscellaneaFunctions extends FunctionCollection{
-	 lazy val lucene = new Lucene
-     val DBPediaDisambiguation3 = "DBPedia-Disambiguation" isDefinedBy {(ec: EvaluationContext,query: String, workMode: String) => lucene.query(ec,query, workMode) }
-	 val DBPediaDisambiguation1 = "DBPedia-Disambiguation" isDefinedBy {(ec: EvaluationContext,query: String) => lucene.query(ec,query) }
-	 val setLangTag = "setLangTag" isDefinedBy {(lit: String, lang: String) => Literal(value = lit, rdfType = XSD_STRING, langTag = lang)}
-	 val matches = "matches" isDefinedBy {(lit:String, re:Regex)=> lit.matches(re.toString())}
-	 val boolean = "boolean" isDefinedBy { (x : Boolean) => x  }
-}
-
-case class VariableReference(variable:Variable) extends Expression{
-  
-  override def evaluate(evaluationContext : EvaluationContext) = evaluationContext.bindings.getValue(variable)
-  override def prettyPrint = variable.toString
-
-}
-
-case class GetRowExpression(variable:Variable) extends Expression {
-    
-    override def evaluate(evaluationContext:EvaluationContext) = Literal(evaluationContext.bindings.getPoint(variable).row, XSD_INT)
-    override def prettyPrint = "get-row(" + variable.toString + ")"
-    
-}
-
-case class GetColExpression(variable:Variable) extends Expression {
-    
-    override def evaluate(evaluationContext:EvaluationContext) = Literal(evaluationContext.bindings.getPoint(variable).col, XSD_INT)
-    override def prettyPrint = "get-col(" + variable.toString + ")"
-    
-}
-
-/*
- * RDF Expresion
- */
-
-case class ResourceExpression(expression:Expression, uri : NamedResource) extends Expression {
-  
-  override def evaluate(evaluationContext : EvaluationContext) = uri + URLEncoder.encode(expression.evaluateAsStringValue(evaluationContext),"UTF-8")
-  override def prettyPrint = "resource(" + expression.toString + "," + uri.toString + ")"
-
-}
-
-case class LiteralExpression(literal : Literal) extends Expression{
-    
-  override def evaluate(evaluationContext : EvaluationContext) = literal
-  override def prettyPrint =  literal.toString
-}
-
-
-
-// FIXME: move this expression to another file
-/*case class RegexExpression(expression : Expression , re : Regex) extends Expression{
-  
-	override def evaluate(evaluationContext : EvaluationContext) =
-	 expression.evaluateAsStringValue(evaluationContext).matches(re.toString()) match{
-	    case true =>  LITERAL_TRUE
-	    case false => LITERAL_FALSE
-	  }
-    override def prettyPrint = "matches(" + expression.toString + ",\"" + re.toString + "\")"
-
-}*/
-
-/*case class DBPediaDisambiguation(expression:Expression) extends Expression{
-  
-  override def evaluate(evaluationContext:EvaluationContext) ={ 
-    
-	
-    //val query = new DBPediaQuery
-	 //query.queryResource(expression.evaluate(evaluationContext))
-    val lucene = new Lucene
-    
-    
-    Literal(lucene.query(""))
-  }
-    override def prettyPrint = "DBPedia-disambiguation(" + expression.toString + ")"
-
-}*/
-
-/*
- * BOOLEAN Expressions 
- */
-case class NotExpression(expression:Expression) extends Expression{
-  
-  override def evaluate(evaluationContext:EvaluationContext) = 
-    
-	 if (expression.evaluateAsTruthValue(evaluationContext))
-	    LITERAL_FALSE
-	 else
-		LITERAL_TRUE
-
-    override def prettyPrint = "not(" + expression.toString + ")"
-
-}
-
-case class TernaryOperationExpression(condition: Expression, trueExpression: Expression, falseExpression : Expression) extends Expression{
-  
-  override def evaluate(evaluationContext: EvaluationContext)= 
-    if(condition.evaluateAsTruthValue(evaluationContext)) trueExpression.evaluate(evaluationContext) else falseExpression.evaluate(evaluationContext)
-  override def prettyPrint = "if" +  condition.toString  +" then "+ trueExpression.toString +" else "+falseExpression.toString 
-}
-
-/* *Type change expressions  * */
-/*case class BooleanExpression(expression: Expression) extends Expression{
-  
-  override def evaluate(evaluationContext : EvaluationContext) =  Literal(expression.evaluateAsTruthValue(evaluationContext), XSD_BOOLEAN)
-  override def prettyPrint = "boolean(" + expression.toString + ")"
-  
-}*/
 
 
 
