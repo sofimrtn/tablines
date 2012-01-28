c8dbeac1338fdc83e2aeceaab787bdb4bb27f464
Diego Berrueta <diego@berrueta.net>
2012-01-28 14:32:24 +0100

    Added functional test

---
+++ tabels-core/src/it/scala/es/ctic/tabels/AbstractFunctionalTest.scala
@@ -0,0 +1,46 @@
+package es.ctic.tabels
+
+import org.scalatest.junit.JUnitSuite
+import org.junit.Test
+import org.junit.Assert._
+import java.io.{File,ByteArrayOutputStream}
+import com.hp.hpl.jena.rdf.model.Model
+import com.hp.hpl.jena.query.{QueryFactory,QueryExecutionFactory}
+import grizzled.slf4j.Logging
+
+abstract class AbstractFunctionalTest extends JUnitSuite with Logging {
+
+    val prefixes = """
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+        PREFIX ex: <http://example.org/>
+        """
+	val program : String
+    val spreadsheets : Seq[String]
+
+    def runTabels() : Model = {
+        val parser = new TabelsParser()
+        val parsedProgram = parser.parseProgram(prefixes + program)
+		val interpreter : Interpreter = new Interpreter()
+		val spreadsheetFiles = spreadsheets map { x => new File(this.getClass().getResource("/" + x).toURI()) }
+		val dataSource : DataSource = new DataAdaptersDelegate(spreadsheetFiles)
+		val dataOutput : JenaDataOutput = new JenaDataOutput(parsedProgram.prefixesAsMap)
+		interpreter.interpret(parsedProgram, dataSource, dataOutput)
+    	return dataOutput.model
+    }
+    
+    def assertAskTrue(model : Model, queryString : String) {
+        val query = QueryFactory.create(prefixes + queryString)
+        val qe = QueryExecutionFactory.create(query, model)
+        assertTrue("Query '" + queryString + "' must return TRUE in model\n" + modelAsString(model), qe.execAsk())
+        qe.close()
+    }
+    
+    def modelAsString(model : Model) : String = {
+        val modelOutput = new ByteArrayOutputStream()
+		model.write(modelOutput, "N3")
+		return modelOutput.toString()
+    }
+    
+}
+
