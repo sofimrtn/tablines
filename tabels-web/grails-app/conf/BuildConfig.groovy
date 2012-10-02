ef030f54d5ce7d15b3983cc17e74797142a8fb69
Carlos Tejo <carlos.tejo@fundacionctic.org>
2012-10-02 16:14:56 +0200

    change folder to generate documentation. Now is web-app/docs

--- tabels-web/grails-app/conf/BuildConfig.groovy
+++ tabels-web/grails-app/conf/BuildConfig.groovy
@@ -2,7 +2,7 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.war.file = "target/${appName}.war"
-grails.project.docs.output.dir = "${appName}/docs"
+grails.project.docs.output.dir = "web-app/docs"
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
