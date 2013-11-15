c5578271188a526e16f60146504b1f3b1f4dd7bb
Alfonso Noriega <alfonso.noriega@fundacionctic.org>
2013-11-15 14:22:46 +0100

    Bug fixed: apache.poi library has a diferent non defined cells and rows than the old library which generated a null pointer exception when reading empty cells

--- tabels-core/src/main/scala/es/ctic/tabels/ExcelDataSource.scala
+++ tabels-core/src/main/scala/es/ctic/tabels/ExcelDataSource.scala
@@ -7,7 +7,7 @@ import scala.collection.JavaConversions._
 import org.apache.poi.hssf.usermodel._
 import grizzled.slf4j.Logging
 import java.text.SimpleDateFormat
-import org.apache.poi.ss.usermodel.{FormulaEvaluator, Workbook, DateUtil,Cell}
+import org.apache.poi.ss.usermodel.{FormulaEvaluator, Workbook, DateUtil,Cell, Row}
 import org.apache.poi.xssf.usermodel.{XSSFFormulaEvaluator, XSSFWorkbook}
 
 
@@ -44,8 +44,22 @@ class ExcelDataAdapter(file : File) extends DataAdapter with Logging {
 	logger.trace("Getting value at " + point)
 	try{
 		val sheet = workbook.getSheet(point.tab)
-		val cell = sheet.getRow(point.row).getCell(point.col )
-	    return ExcelCellValue(cell, evaluator)
+    val row = sheet.getRow(point.row)
+    //If the row is not defined poi will give us a null value
+    row match{
+      case null =>
+      {
+        //We create an empty row and cell so we can get an empty cell type and not a null value when getting the cell content
+        val newTempCell: Cell =  sheet.createRow(point.row).createCell(point.col,Cell.CELL_TYPE_BLANK)
+        ExcelCellValue(newTempCell, evaluator)
+      }
+      case _:Row =>
+      {
+          //Row.CREATE_NULL_AS_BLANK defines missing cell policy
+          val cell = row.getCell(point.col, Row.CREATE_NULL_AS_BLANK)
+  	      ExcelCellValue(cell, evaluator)
+      }
+  }
 	}catch{
 	  case e=>throw new IndexOutOfBounds(point)
 	}
@@ -76,44 +90,59 @@ case class ExcelCellValue (cell:Cell, evaluator: FormulaEvaluator) extends CellV
   override def getContent : Literal ={
     logger.trace("Actual cell type is: " + cell.getCellType() + " and its value is: "+cell)
     cell.getCellType() match{
-	  case Cell.CELL_TYPE_NUMERIC =>
-                if (DateUtil.isCellDateFormatted(cell)){
-                  val xsdDateFormater = new SimpleDateFormat("yyyy-MM-dd")
-                  val value = xsdDateFormater.format(cell.getDateCellValue)
-                  Literal(value, XSD_DATE)
+      case Cell.CELL_TYPE_NUMERIC =>
+      {           logger.trace("Accesing the Numeric Type Cell: Row - "+ cell.getRowIndex + ", Col - "+ cell.getColumnIndex )
+                  if (DateUtil.isCellDateFormatted(cell)){
+                    val xsdDateFormater = new SimpleDateFormat("yyyy-MM-dd")
+                    val value = xsdDateFormater.format(cell.getDateCellValue)
+                    Literal(value, XSD_DATE)
 
-                } else{
-                  val value = cell.getNumericCellValue
-                  if(value == value.toInt)
-                    Literal(value.toInt, XSD_INTEGER)
-                  else Literal(value.toString, XSD_DOUBLE)
-                }
-	  case Cell.CELL_TYPE_STRING =>Literal(cell.getRichStringCellValue, XSD_STRING)
-	  case Cell.CELL_TYPE_BOOLEAN => Literal(cell.getBooleanCellValue().toString(), XSD_BOOLEAN)
-	  case Cell.CELL_TYPE_FORMULA =>{
-        val evaluatedCell=  evaluator.evaluate(cell)
-        evaluatedCell.getCellType match{
-           case Cell.CELL_TYPE_NUMERIC =>
-             //FIXME date formula not recogniced as date
-            /* if (DateUtil.isCellDateFormatted(new HSSFCell(cell.getSheet, evaluatedCell))){
-               val xsdDateFormater = new SimpleDateFormat("yyyy-MM-dd")
-               //val value = xsdDateFormater.format(evaluatedCell.getJavaDate)
-               Literal(evaluatedCell.toString, XSD_DATE)
-             }else{ */
-               val value = evaluatedCell.getNumberValue
-               if(value == value.toInt)
-                 Literal(value.toInt, XSD_INTEGER)
-               else Literal(value.toString, XSD_DOUBLE)
-             //}
-           case Cell.CELL_TYPE_STRING => Literal(evaluatedCell.getStringValue, XSD_STRING)
-           case Cell.CELL_TYPE_BOOLEAN => Literal(evaluatedCell.getBooleanValue().toString(), XSD_BOOLEAN)
-           case Cell.CELL_TYPE_BLANK => Literal("", XSD_STRING)
-           case Cell.CELL_TYPE_ERROR => Literal("", XSD_STRING)
+                  } else{
+                    val value = cell.getNumericCellValue
+                    if(value == value.toInt)
+                      Literal(value.toInt, XSD_INTEGER)
+                    else Literal(value.toString, XSD_DOUBLE)
+                  }
+      }
+      case Cell.CELL_TYPE_STRING =>
+      { logger.trace("Accesing the String Type Cell: Row - "+ cell.getRowIndex + ", Col - "+ cell.getColumnIndex )
+        Literal(cell.getRichStringCellValue, XSD_STRING)
+      }
+      case Cell.CELL_TYPE_BOOLEAN =>
+      { logger.trace("Accesing the Boolean Type Cell: Row - "+ cell.getRowIndex + ", Col - "+ cell.getColumnIndex )
+        Literal(cell.getBooleanCellValue().toString(), XSD_BOOLEAN)
+      }
+      case Cell.CELL_TYPE_FORMULA =>
+      {   logger.trace("Accesing the Formula Type Cell: Row - "+ cell.getRowIndex + ", Col - "+ cell.getColumnIndex )
+          val evaluatedCell=  evaluator.evaluate(cell)
+          evaluatedCell.getCellType match{
+             case Cell.CELL_TYPE_NUMERIC =>
+               //FIXME date formula not recogniced as date
+              /* if (DateUtil.isCellDateFormatted(new HSSFCell(cell.getSheet, evaluatedCell))){
+                 val xsdDateFormater = new SimpleDateFormat("yyyy-MM-dd")
+                 //val value = xsdDateFormater.format(evaluatedCell.getJavaDate)
+                 Literal(evaluatedCell.toString, XSD_DATE)
+               }else{ */
+                 val value = evaluatedCell.getNumberValue
+                 if(value == value.toInt)
+                   Literal(value.toInt, XSD_INTEGER)
+                 else Literal(value.toString, XSD_DOUBLE)
+               //}
+             case Cell.CELL_TYPE_STRING => Literal(evaluatedCell.getStringValue, XSD_STRING)
+             case Cell.CELL_TYPE_BOOLEAN => Literal(evaluatedCell.getBooleanValue().toString(), XSD_BOOLEAN)
+             case Cell.CELL_TYPE_BLANK => Literal("", XSD_STRING)
+             case Cell.CELL_TYPE_ERROR => Literal("", XSD_STRING)
+             }
            }
-         }
-    case Cell.CELL_TYPE_BLANK => Literal("", XSD_STRING)
-    case Cell.CELL_TYPE_ERROR => Literal("", XSD_STRING)
-	}
-    }
+      case Cell.CELL_TYPE_BLANK =>
+      { logger.trace("Accesing the Blank Type Cell: Row - "+ cell.getRowIndex + ", Col - "+ cell.getColumnIndex )
+        Literal("", XSD_STRING)
+      }
+      case Cell.CELL_TYPE_ERROR =>
+      { logger.trace("Accesing the Error Type Cell: Row - "+ cell.getRowIndex + ", Col - "+ cell.getColumnIndex )
+        Literal("", XSD_STRING)
+      }
+	  }
+  }
 
 }
