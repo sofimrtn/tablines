797f35518a578518997f401569a2566aa24cbefd
Alfonso Noriega <alfonso.noriega@fundacionctic.org>
2013-11-18 11:00:30 +0100

    Added info about dependency licenses

--- tabels-web/grails-app/conf/BuildConfig.groovy
+++ tabels-web/grails-app/conf/BuildConfig.groovy
@@ -39,19 +39,19 @@ grails.project.dependency.resolution = {
         
     }
     plugins {
-        runtime ':uploadr:0.6.0',
+        runtime ':uploadr:0.6.0',// Apache Lincese version 2.0
 				//':tapinos-js:1.7',
-                ':svn:1.0.2',
+                ':svn:1.0.2',// Apache Lincese version 2.0
                 //Added for tapinos-ws
-                ":jquery:1.7.1",
-                ":fancybox:1.3.4",
+                ":jquery:1.7.1",//MIT License
+                ":fancybox:1.3.4",//MIT License
                 ":jquery-geturlparam:2.1",
                 ":jquery-datatables:1.8.2",
                 ":jquery-tipsy:1.0.0a",
                 ":jquery-tooltip:1.3",
                 ":jquery-ui:1.8.15",
-                ":resources:1.1.6",
-                ":mail:1.0"
+                ":resources:1.1.6",// Apache Lincese version 2.0
+                ":mail:1.0"// Apache Lincese version 2.0
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
@@ -68,20 +68,20 @@ grails.project.dependency.resolution = {
         compile('es.ctic.understats:understats-ws:2.6-SNAPSHOT'){
             excludes 'arq'
         }
-        compile('commons-lang:commons-lang:2.6')
-        compile('commons-io:commons-io:2.1')
-        compile('commons-httpclient:commons-httpclient:3.0')
-        compile('org.scala-lang:scala-library:2.9.2')
-        compile('org.fundacionctic.su4j:su4j-endpoint:1.2') {
+        compile('commons-lang:commons-lang:2.6') // Apache Lincese version 2.0
+        compile('commons-io:commons-io:2.1')  // Apache Lincese version 2.0
+        compile('commons-httpclient:commons-httpclient:3.0') // Apache Lincese version 2.0
+        compile('org.scala-lang:scala-library:2.9.2') //BSD like license
+        compile('org.fundacionctic.su4j:su4j-endpoint:1.2')// Apache Lincese version 2.0 {
             excludes 'jena', 'arq', 'jena-core', 'jena-arq', 'jena-tdb', 'slf4j-log4j12', 'slf4j-jdk14'
         }
         compile('org.fundacionctic.ext:pubby:0.3.3.1') {
             excludes 'arq'
         }
-        compile('com.hp.hpl.jena:jena:2.6.4') {
+        compile('com.hp.hpl.jena:jena:2.6.4') // BSD License {
             excludes 'slf4j-log4j12'
         }
-        runtime('com.hp.hpl.jena:arq:2.8.8') { // transitive dependency, added here because we exclude it above
+        runtime('com.hp.hpl.jena:arq:2.8.8') // BSD like license { // transitive dependency, added here because we exclude it above
             excludes 'slf4j-log4j12',  'stax-api'
         }
     }
